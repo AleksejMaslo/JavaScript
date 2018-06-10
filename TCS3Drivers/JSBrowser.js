14c4acd6588609087961a0516cf5a8ae2f98f1b1
Aleksej Maslo <aleksej.maslodp@gmail.com>
2018-06-10 23:27:42 +0300

    Adding driversRequest

---
+++ TCS3Drivers/JSBrowser.js
@@ -0,0 +1,72 @@
+"use strict";
+
+function addDrivers() {
+    var idValue = document.getElementById('id').value;
+    var nameValue = document.getElementById('name').value;
+    var licenseValue = document.getElementById('license').value;
+    var rateValue = document.getElementById('rate').value;
+    var countryElement = document.getElementById("country");
+    var countryValue = country.options[country.selectedIndex].value;
+
+    var newDriver = {
+        id: idValue, name: nameValue, license: licenseValue, rate: rateValue
+    };
+
+
+    console.log("drivers " + JSON.stringify(newDriver));
+    addDriverRequest(newDriver, countryValue);
+}
+
+function addDriverRequest(newDriver, countryValue) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", '/drivers', true);
+
+    xhr.onreadystatechange = function() {//Call a function when the state changes.
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            console.log("adding drivers success");// Request finished. Do processing here.
+        } else {
+            console.log("Error");
+        }
+    };
+    xhr.send(JSON.stringify(newDriver));
+}
+
+function sendRequest(locale) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            var tableHeader =
+                "<tr>" +
+                "<th>" + "id" + "</th>" +
+                "<th>" + "name" + "</th>" +
+                "<th>" + "license" + "</th>" +
+                "<th>" + "rate" + "</th>" +
+                "</tr>";
+            var tableContent = "";
+            for (var i = 0; i < obj.drivers.length; i++) {
+                console.log(obj.drivers[i]);
+                tableContent = tableContent + "<tr>" +
+                    "<td>" + obj.drivers[i].id + "</td>" +
+                    "<td>" + obj.drivers[i].name + "</td>" +
+                    "<td>" + obj.drivers[i].license + "</td>" +
+                    "<td>" + obj.drivers[i].rate + "</td>" +
+                    "</tr>";
+            }
+            var table = "<table>" + tableHeader + tableContent + "</table>";
+            document.getElementById("drivers").innerHTML = table;
+        }
+    };
+    xhttp.open("GET", "taxiDrivers_" + locale + ".json", true);
+    xhttp.send();
+}
+
+function loadUADrivers() {
+    sendRequest("UA");
+}
+
+function loadENDrivers() {
+    sendRequest("EN");
+}
+
+sendRequest("EN");
---
+++ TCS3Drivers/JSServer.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var jsContent = fs.readFileSync('JSBrowser.js', 'utf8');
+var html = fs.readFileSync('HTML3.html', 'utf8');
+var css = fs.readFileSync("Table.css", 'utf8');
+var contentsEN = fs.readFileSync('taxiDrivers_EN.json', 'utf8');
+var contentsUA = fs.readFileSync('taxiDrivers_UA.json', 'utf8');
+var http = require('http');
+http.createServer(function (req, res) {
+    if (req.url === "/JSBrowser.js") {
+        res.write(jsContent);
+    } else if (req.url === "/Table.css") {
+      res.write(css);
+    } else if (req.url === "/taxiDrivers_EN.json") {
+        res.write(contentsEN);
+    } else if (req.url === "/taxiDrivers_UA.json") {
+        res.write(contentsUA);
+    } else if (req.url === "/drivers" && req.method === "POST") {
+        console.log("Accept adding driver request");
+        var body = '';
+        req.on('data', function (chunk) {
+            body += chunk.toString(); // convert Buffer to string
+        });
+        req.on('end', function () {
+            console.log(body);
+            res.end('ok');
+            //Здесь нужно обработать body и сохранить в файл
+        });
+    } else {
+        res.write(html);
+    }
+    res.end();
+}).listen(8080);
