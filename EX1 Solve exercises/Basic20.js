4cce9dd78d25fb151f716bd4b2ee5a5eb7dd2f39
Aleksej Maslo <aleksej.maslodp@gmail.com>
2018-06-14 13:05:03 +0300

    Array sort

--- EX1 Solve exercises/Basic20.js
+++ EX1 Solve exercises/Basic20.js
@@ -6,7 +6,7 @@ function check(x, y) {
     } else if (x < 0 && y > 0) {
         console.log("Negative value x " + "Positive value y");
     } else {
-        console.log("Negative value x " + "Positive value y");
+        console.log("Negative value x " + "Negative value y");
     }
 }
 
--- EX1 Solve exercises/Basic21.js
+++ EX1 Solve exercises/Basic21.js
@@ -1,6 +1,11 @@
 function string(x) {
-
+    if (x.substr(0, 2) === "Py") {
+        console.log(x);
+    } else {
+        console.log("Py" + x);
+    }
 }
 
-console.log(string(""));
+console.log(string("Pye"));
+console.log(string("e"));
 console.log(string(""));
